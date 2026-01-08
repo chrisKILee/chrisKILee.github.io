0ac8d4c5228776c6a3789d096915cbbe6b5fc4a1
Chris Lee <bitjjangi@gmail.com>
2026-01-08 10:04:38 +0900

    The Observability Stack Layers

--- gemini_html/02_work/files_config.js
+++ gemini_html/02_work/files_config.js
@@ -9,7 +9,9 @@ window.FOLDER_CONFIG['02_work'] = {
         "changwon.html",
         "changwon-process.html",
         "seah_css.html",
-        "opsdev.html"
+        "opsdev.html",
+        "cursor_teams.html",
+        "Observability_Stack_Layers.html"
     ],
     "changwon.html": "창원특수강 공정",
     "changwon-process.html": "창원특수강 공정 v2",
@@ -19,5 +21,6 @@ window.FOLDER_CONFIG['02_work'] = {
     "platfor_provider-service_operator.html": "차세대플렛폼 역할구분",
     "service-dev-lifecycle-guide-2nd.html": "서비스 개발 라이프사이클 가이드",
     "platfor_provider-service_operator-infra_enabler.html": "R&D실, 인프라팀, 지역센터의 역할",
-    "opsdev.html": "OpsDeveloper 란?"
+    "opsdev.html": "OpsDeveloper 란?",
+    "Observability_Stack_Layers.html": "Observability Stack Layers"
 };
