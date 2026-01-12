0b06c7aa0d97350c6e083a88001d71cd0afb9818
Chris Lee <bitjjangi@gmail.com>
2026-01-13 08:59:51 +0900

    [add page] ai 와 정보보호

--- gemini_html/06_VNTG_AI_STUDY/files_config.js
+++ gemini_html/06_VNTG_AI_STUDY/files_config.js
@@ -3,8 +3,10 @@ window.FOLDER_CONFIG['06_VNTG_AI_STUDY'] = {
     "_folderName": "🚀 VNTG AI SDD",
     "_order": [
         "sdd.html",
-        "ai_model_compare.html"
+        "ai_model_compare.html",
+        "ai_security.html"
     ],
     "sdd.html": "SDD 설명",
-    "ai_model_compare.html": "AI 모델 비교"
+    "ai_model_compare.html": "AI 모델 비교",
+    "ai_security.html": "AI 와 정보보호 거버넌스"
 };
