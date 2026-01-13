7579759b714a0ab092b4bd74c7ea149cec49494e
Chris Lee <bitjjangi@gmail.com>
2026-01-13 10:35:14 +0900

    [add page] ai 용어 가이드

--- gemini_html/06_VNTG_AI_STUDY/files_config.js
+++ gemini_html/06_VNTG_AI_STUDY/files_config.js
@@ -2,11 +2,15 @@ window.FOLDER_CONFIG = window.FOLDER_CONFIG || {};
 window.FOLDER_CONFIG['06_VNTG_AI_STUDY'] = {
     "_folderName": "🚀 VNTG AI SDD",
     "_order": [
+        "rag_ai_dev_guide.html",
         "sdd.html",
         "ai_model_compare.html",
-        "ai_security.html"
+        "ai_security.html",
+        "vntg_ai_study.html"
     ],
+    "rag_ai_dev_guide.html": "RAG AI 개발 가이드",
     "sdd.html": "SDD 설명",
     "ai_model_compare.html": "AI 모델 비교",
-    "ai_security.html": "AI 와 정보보호 거버넌스"
+    "ai_security.html": "AI 와 정보보호 거버넌스",
+    "vntg_ai_study.html": "AI 용어 가이드"
 };
