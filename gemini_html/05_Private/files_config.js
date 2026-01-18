582700c2db860c790a18c5988d69ebe32db50c20
Chris Lee <bitjjangi@gmail.com>
2026-01-18 16:26:12 +0900

    [add page] score board

--- gemini_html/05_Private/files_config.js
+++ gemini_html/05_Private/files_config.js
@@ -5,10 +5,12 @@ window.FOLDER_CONFIG['05_Private'] = {
         "travel_api.html",
         "Remote_Access_wol.html",
         "4700U.html",
-        "android-all-in-one_with_knob.html"
+        "android-all-in-one_with_knob.html",
+        "scoreboard.html"
     ],
     "travel_api.html": "Travel API",
     "Remote_Access_wol.html": "Remote Access - Wake on Lan",
     "4700U.html": "Genmachine Ren 4700U 튜닝",
-    "android-all-in-one_with_knob.html": "차량용 안드로이드: 노브 휠 컨트롤러 UX 연구"
+    "android-all-in-one_with_knob.html": "차량용 안드로이드: 노브 휠 컨트롤러 UX 연구",
+    "scoreboard.html": "5인용 실시간 점수판 v2"
 };
